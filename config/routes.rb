03822c9c9fadc9eff41d0367d2389f675f4d6918
Pablo <pablochavezing@gmail.com>
2021-05-15 09:52:07 -0700

    Configuracion inicial de las rutas de la aplicacion

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'projects/index'
   get 'projects/create'
   get 'projects/dashboard'
+  root 'projects#dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
