9d452028b92ddc401a9bc07fba3c1c4383537602
Pablo <pablochavezing@gmail.com>
2021-05-15 09:49:19 -0700

    Creacion del controlador Projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'projects/index'
+  get 'projects/create'
+  get 'projects/dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
