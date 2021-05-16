527905343b06e1fccb7ce10f788eea10c483d45b
Pablo <pablochavezing@gmail.com>
2021-05-16 10:10:28 -0700

    Agregando el cdn de bootstrap y actualizacion de las vistas

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,23 @@
 class ProjectsController < ApplicationController
+  /http_basic_authenticate_with name: "desafiovamoscontodo", password: "XAHTJEAS23123%23", only: :dashboard/
+
   def index
+    @projects = Project.all
   end
 
   def create
+    /Se pasa la base de datos de Projects a la variable de instancia/
+    @projects = Project.create(name: params[:name], description: params[:description], start_date: params[:start_date], end_date: params[:end_date])
   end
 
   def dashboard
+    @projects = Project.all.order(created_at: :desc)
+
+    if params[:name].present?
+      @projects = Project.where('name = ?', params[:name])
+    else
+      @projects = Project.find_each
+    end
   end
+  
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,11 @@
 class Project < ApplicationRecord
+    /validar los campos/ 
+    validates :name, :description, :start_date, :end_date, presence: true
+
+    /Eliminar cierta parabra que no queramos/
+    /before_save :replace_word
+
+    def replace_word
+        self.content = self.content.gsub "spoiler", " "
+    end/
 end
